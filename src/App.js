efbb97e4325745a88ce3fa5c14b57d6c3b29abde
relaxin07 <relaxinfh@mail.ru>
2020-05-01 03:18:15 +0300

    change classname

--- src/App.js
+++ src/App.js
@@ -4,19 +4,9 @@ import Bakeries from './components/Bakeries/BakeriesContainer'
 import LocationsContainer from './components/Locations/LocationsContainer'
 
 class App extends React.Component {
-  script = async () => {
-    const res = await fetch('http://api.dev.cakeiteasy.no/api/store/bakeries/?country_code=no')
-    const response = await res.json()
-
-    const scheduleAll = response.map(({ schedule }) => schedule)
-    let newArr = []
-   
-    console.log(newArr)
-  }
   render() {
-    this.script()
     return (
-      <div className="App">
+      <div className="container">
         <Bakeries />
         <LocationsContainer />
       </div>
