ff9ca370975bd7d170e74cdad15965d096728d07
relaxin07 <relaxinfh@mail.ru>
2020-05-01 03:23:58 +0300

    add try catch in thunk

--- src/store/modules/bakers/index.js
+++ src/store/modules/bakers/index.js
@@ -2,47 +2,33 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import BakeriesApi from '../../../api/baker-service'
 
 const getBakeriesAsync = createAsyncThunk('bakeries/getBakeriesStatus', async (_, { getState }) => {
-  return BakeriesApi.handlerGetBakeries()
-    .then((list) => {
-      return list
-    })
-    .catch((e) => {
-      return `${e} + 'error'`
-    })
+  try {
+    return await BakeriesApi.handlerGetBakeries()
+  } catch (e) {
+    console.log(e)
+  }
 })
 const getCitiesAsync = createAsyncThunk('bakeries/getCitiesStatus', async (_, { getState }) => {
-  return BakeriesApi.handlerGetCities()
-    .then((list) => {
-      return list
-    })
-    .catch((e) => {
-      return `${e} + 'error`
-    })
+  try {
+    return await BakeriesApi.handlerGetCities()
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 const bakersSlice = createSlice({
   name: 'bakeries',
   initialState: {
-    cities: {
-      citiesList: [],
-      error: null,
-      isLoading: false,
-    },
-    bakeries: {
-      bakeriesList: [],
-      error: null,
-      isLoading: false,
-    },
+    cities: [],
+    bakeries: [],
   },
   extraReducers: {
     [getCitiesAsync.fulfilled]: (state, action) => {
-      state.cities.citiesList = action.payload
-      state.cities.isLoading = true
+      state.cities = action.payload
     },
 
     [getBakeriesAsync.fulfilled]: (state, action) => {
-      state.bakeries.bakeriesList = action.payload
-      state.bakeries.isLoading = true
+      state.bakeries = action.payload
     },
   },
 })
